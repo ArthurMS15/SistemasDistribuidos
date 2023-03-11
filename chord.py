f3ed3ea8790e49c5c375c210e302c44b3a385302
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-03-11 12:03:12 -0300

    Update chord.py

--- chord.py
+++ chord.py
@@ -1,22 +1,7 @@
-import hashlib #importa o módulo de hash SHA1
+from hashlib import sha1 #importa o módulo de hash SHA1
 
-class Node: #para criação do nodo
-    def __init__(self, node_id, address):
-        self.node_id = node_id
-        self.address = address
-        self.predecessor = None
-        self.successor = None
-        self.data = {}
-    
-    def find_successor(self, key):
-        if self.successor.node_id == self.node_id or self.successor.node_id >= key:
-            return self.successor
-        else:
-            node = self.closest_preceding_node(key)
-            return node.find_successor(key)
-    
-    def closest_preceding_node(self, key):
-        for i in range(len(self.node_id)-1, -1, -1):
-            if self.node_id[i] < key[i]:
-                return self.successor if self.successor.node_id != self.node_id else self
-        return self
+class Node:
+    def __init__(self, id, address):
+        self.id = id  #hash SHA-1 do endereço)
+        self.address = address  #string no formato "ip:porta")
+        self.data = {}  #dicionário que guarda os dados armazenados no nó
