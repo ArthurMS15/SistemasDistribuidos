e5e5d6e9f9ae4230814cc350ce023de7b57d1376
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-03-11 12:04:03 -0300

    Update chord.py

--- chord.py
+++ chord.py
@@ -5,3 +5,11 @@ class Node:
         self.id = id  #hash SHA-1 do endereço)
         self.address = address  #string no formato "ip:porta")
         self.data = {}  #dicionário que guarda os dados armazenados no nó
+
+    #insere um par chave-valor no dicionário de dados
+    def insert_data(self, key, value):
+        self.data[key] = value
+
+    #busca valor associado a uma chave no dicionário de dados
+    def lookup_data(self, key):
+        return self.data.get(key)
