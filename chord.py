7a7e1ab566f923c0cd79a474412b562f302fa6eb
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-03-11 12:15:21 -0300

    Update chord.py

--- chord.py
+++ chord.py
@@ -13,3 +13,16 @@ class Node:
     #busca valor associado a uma chave no dicionário de dados
     def lookup_data(self, key):
         return self.data.get(key)
+
+class Chord:
+    def __init__(self, n_bits):
+        self.n_bits = n_bits  #número de bits dos identificadores de nó
+        self.nodes = []  #lista que guarda os nós na rede
+
+    def add_node(self, address):
+        # Calcula o identificador do nó como o hash SHA-1 do endereço
+        id = int(sha1(address.encode()).hexdigest(), 16) % (2 ** self.n_bits)
+        node = Node(id, address)
+        self.nodes.append(node)
+        self.nodes.sort(key=lambda n: n.id)  #ordena a lista de nós pelo id
+        return node
