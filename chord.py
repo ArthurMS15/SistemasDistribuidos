f8c4b83bb9b16eb491c7049a001a7b96abec8efb
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-03-11 13:31:42 -0300

    Update chord.py

--- chord.py
+++ chord.py
@@ -2,8 +2,8 @@ from hashlib import sha1 #importa o módulo de hash SHA1
 
 class Node:
     def __init__(self, id, address):
-        self.id = id  #hash SHA-1 do endereço)
-        self.address = address  #string no formato "ip:porta")
+        self.id = id  #hash SHA-1 do endereço
+        self.address = address  #string no formato "ip:porta"
         self.data = {}  #dicionário que guarda os dados armazenados no nó
 
     #insere um par chave-valor no dicionário de dados
