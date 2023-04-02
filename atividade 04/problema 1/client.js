dc3283695cb357be9946421d45ae8b03ead87a4e
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-04-02 12:24:52 -0300

    Comentários

--- atividade 04/problema 1/client.js
+++ atividade 04/problema 1/client.js
@@ -2,41 +2,45 @@ const net = require("net");
 const fs = require("fs");
 const process = require("process");
 
+/*Importa o módulo net, necessário para criar um cliente de rede e se comunicar com o servidor.
+Importa o módulo fs, usado para criar e manipular arquivos no sistema de arquivos local.
+Importa o módulo process, usado para acessar argumentos da linha de comando e sair do programa.*/
+
 if (process.argv.length !== 5) {
-  console.log("Uso: node client.js <ip_servidor> <porta> <nome_arquivo>");
+  console.log("Uso: node client.js <ip_servidor> <porta> <nome_arquivo>");  //quantidade correta de argumentos foi fornecia
   process.exit(1);
 }
 
-const serverIP = process.argv[2];
+const serverIP = process.argv[2]; //armazena o segundo argumento
 const serverPort = process.argv[3];
 const fileName = process.argv[4];
 
-const client = new net.Socket();
+const client = new net.Socket();  //cria um novo objeto de socket para o cliente.
 client.connect(serverPort, serverIP, () => {
   console.log("Conectado ao servidor");
   client.write(fileName);
-});
+}); //conecta-se ao servidor no IP e porta especificados e define uma função de callback a ser chamada quando a conexão for estabelecida.
 
-client.on("data", (data) => {
-  const response = data.toString();
+client.on("data", (data) => { //define uma função de callback para lidar com os dados recebidos do servidor.
+  const response = data.toString(); //converte os dados recebidos em uma string.
   if (response.startsWith("EXIST")) {
     const fileSize = parseInt(response.split(" ")[1]);
     console.log(`Arquivo encontrado. Tamanho: ${fileSize}`);
     client.write("OK");
 
-    const fileStream = fs.createWriteStream(fileName);
+    const fileStream = fs.createWriteStream(fileName); //cria um novo arquivo usando o nome fornecido e cria um fluxo de gravação para o arquivo.
     let receivedBytes = 0;
     client.on("data", (chunk) => {
       receivedBytes += chunk.length;
       const progress = (receivedBytes / fileSize) * 100;
-      console.log(`Recebendo arquivo... ${progress.toFixed(2)}%`);
+      console.log(`Recebendo arquivo... ${progress.toFixed(2)}%`); //Calcule e exibe o progresso da transferência do arquivo. 
       fileStream.write(chunk);
 
       if (receivedBytes === fileSize) {
         console.log("Arquivo recebido com sucesso.");
         client.destroy();
       }
-    });
+    }); // Define uma função de callback para lidar com os dados recebidos (chunks) do servidor.
   } else {
     console.log("Arquivo não encontrado.");
     client.destroy();
--- atividade 04/problema 1/server.py
+++ atividade 04/problema 1/server.py
@@ -1,35 +1,35 @@
-import socket
+import socket #módulo necessário para ciraçaão da comunicação cliente-servidor
 import sys
-import os
+import os #manipulação de caminhos de arquivos e diretórios.
 
 def main():
     if len(sys.argv) != 3:
         print("Uso: python server.py <porta> <diretorio>")
-        sys.exit(1)
+        sys.exit(1)                                         #verifica a quantidade correta de argumentos fornecida
 
-    port = int(sys.argv[1])
+    port = int(sys.argv[1])                                 #converte para inteiro o segundo argumento (porta)
     directory = sys.argv[2]
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('', port))
-    server.listen(1)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria um novo objeto de socket usando o protocolo IPv4 (AF_INET) e TCP (SOCK_STREAM). 
+    server.bind(('', port))                                    #cincula o socket ao endereço e porta especificados
+    server.listen(1)                                           #servidor em modo de escuta, aguardando conexões de clientes
     print(f"Servidor escutando na porta {port}...")
 
     while True:
-        conn, addr = server.accept()
+        conn, addr = server.accept()                           #aceita uma conexão de cliente e retorna um novo objeto de socket (conn) e o endereço do cliente (addr).
         print(f"Conexão estabelecida com {addr}")
-        filename = conn.recv(1024).decode()
-        filepath = os.path.join(directory, filename)
+        filename = conn.recv(1024).decode()                    #recebe dados do cliente (tamanho máximo de 1024 bytes) e decodifica-os para obter o nome do arquivo.
+        filepath = os.path.join(directory, filename)           #combina o diretório fornecido e o nome do arquivo para criar o caminho completo do arquivo.
 
-        if os.path.isfile(filepath):
-            conn.send(b"EXIST " + str(os.path.getsize(filepath)).encode())
-            user_response = conn.recv(1024).decode()
+        if os.path.isfile(filepath):                           #verifica se o arquivo existe no caminho especificado.
+            conn.send(b"EXIST " + str(os.path.getsize(filepath)).encode()) #envia a mensagem "EXIST" e o tamanho do arquivo em bytes para o cliente.
+            user_response = conn.recv(1024).decode()           #recebe a resposta do cliente e a decodifica
 
             if user_response == "OK":
-                with open(filepath, "rb") as f:
+                with open(filepath, "rb") as f:                #modo de leitura binária 
                     bytes_read = f.read(4096)
                     while bytes_read:
-                        conn.send(bytes_read)
+                        conn.send(bytes_read)                  #envia bytes lidos para o cliente
                         bytes_read = f.read(4096)
                 print("Arquivo enviado com sucesso.")
         else:
